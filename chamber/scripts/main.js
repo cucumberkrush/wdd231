6c25b9c1b5180e5058b3aa94513478bf60e3fe50
cucumberkrush <mchukwu@byupathway.edu>
2025-06-19 20:12:39 +0100

    deleted to start all over

--- chamber/scripts/main.js
+++
@@ -1,3 +0,0 @@
-// Update footer info
-document.getElementById('current-year').textContent = new Date().getFullYear();
-document.getElementById('last-modified').textContent = `Last Updated: ${document.lastModified}`;
--- scripts/app.js
+++
@@ -1,3 +0,0 @@
-// No need for DOMContentLoaded when scripts are at end of body
-document.getElementById('copyright-year').textContent = new Date().getFullYear();
-document.getElementById('last-modified').textContent = `Last Modified: ${document.lastModified}`;
